4041e0caa16d4b9ecee5cd0c68a9aab36aa423e9
zbetzen <zbetzen@gmail.com>
2025-10-02 20:58:03 -0600

    changed description

--- src/pages/equipment/Relics.js
+++ src/pages/equipment/Relics.js
@@ -7,7 +7,7 @@ const Relics = () => {
     <p>A wooden trinket the size of a hand with images of the Crone engraved on it's surface.</p>
     <p>This allows you to begin each session with 3 luck dice and at any time donate your luck dice to another character.</p>
     <h2>Blasting Rod</h2>
-    <p>A four foot wooden scepter engraved with prayers to the Sky Father and capped with a fist sized fulgurite glass knob.</p>
+    <p>A four foot wooden scepter capped with a fist sized fulgurite glass knob.</p>
     <p>The rod can be used as a meele weapon using the d10 effort die. However you may spend luck dice as additional damage in the form crackling sparks and lightening.</p>
   </div>;
 };
